f999000d217461371e8da8fbdeda0b579ec0ad1e
Strelok90 <73306594+Strelok90@users.noreply.github.com>
2020-10-26 09:37:44 +0300

    Update lesson1_2.java
    public class lesson
    лучше:
    public class Lesson

--- lesson1_2.java
+++ lesson1_2.java
@@ -1,6 +1,6 @@
 package home.work;
 
-public class lesson2 {
+public class Lesson2 {
     public static void main(String[] args) {
         // Создать переменные всех пройденных типов данных, и инициализировать их значения;
         byte valByte = 120;
